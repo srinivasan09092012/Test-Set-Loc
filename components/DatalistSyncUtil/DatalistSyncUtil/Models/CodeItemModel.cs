951d3acaca610aa5d5452673cc67957b606db769
upadhami <upadhami@asiapacific.tfs.local>
2017-05-05 09:15:49 +0000

    servicecommented code
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1430319

--- components/DatalistSyncUtil/DatalistSyncUtil/Models/CodeItemModel.cs
+++ components/DatalistSyncUtil/DatalistSyncUtil/Models/CodeItemModel.cs
@@ -6,7 +6,6 @@
 //-----------------------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
-using DatalistSyncUtil.DataListsService;
 
 namespace DatalistSyncUtil
 {
