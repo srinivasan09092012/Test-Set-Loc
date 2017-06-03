f25f2280efdf03bb0fb3d4a0d2fec9a336e1699e
raosrip <sripathi-r.rao@dxc.com>
2017-06-03 21:15:54 +0000

    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1437128

--- components/SSRSImportExportWizard/SSRSImportExportWizard/ImportReportView.cs
+++ components/SSRSImportExportWizard/SSRSImportExportWizard/ImportReportView.cs
@@ -2,16 +2,11 @@
 using SSRSImportExportWizard.ReportServer2010;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
-using System.Xml.Linq;
 
 namespace SSRSImportExportWizard
 {
