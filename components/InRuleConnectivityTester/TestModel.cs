3bf7a7a5e309d431b888fbb957276177e9e2c2a6
mgoppert <mike.goppert@dxc.com>
2017-08-16 21:12:50 +0000

    Initial code.
    Workitems:
    #528197 More Descriptive Error Msg when Dates Outside Min/Max Values 
    #531600 Code Review for User Story 528197: More Descriptive Error Msg when Dates Outside Min/Max Values
    #533205 BRE Copy Assembly Files
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1455175

---
+++ components/InRuleConnectivityTester/TestModel.cs
@@ -0,0 +1,35 @@
+﻿//--------------------------------------------------------------------------------------------------
+// This code is the property of Hewlett Packard Enterprise, Copyright (c) 2016. All rights reserved.
+//
+// Any unauthorized use in whole or in part without written consent is strictly prohibited.
+// Violators may be punished to the full extent of the law.
+//--------------------------------------------------------------------------------------------------
+using HPE.HSP.UA3.Core.API.BusinessRules.Interface;
+using HPE.HSP.UA3.Core.API.BusinessRules.Interface.Domain;
+using System;
+
+namespace HPE.HSP.UA3.Core.API.BusinessRules.Providers.Tests.Domain
+{
+    [Serializable]
+    public class TestModel : IBusinessRulesObject
+    {
+        public TestModel()
+        {
+            this.Rules = new BaseValidationModel();
+        }
+
+        public TestModel(string name, int priority) : this()
+        {
+            this.Name = name;
+            this.Priority = priority;
+        }
+
+        public string Name { get; set; }
+
+        public int Priority { get; set; }
+
+        public int Validation { get; set; }
+
+        public BaseValidationModel Rules { get; set; }
+    }
+}
