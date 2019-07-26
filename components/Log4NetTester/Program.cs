f0ba1c72e5baab6162b7f5403774518e14e9dd0d
Mike Goppert <mike.goppert@gainwelltechnologies.com>
2019-07-26 11:35:52 +0000

    Initial code.
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1563864

---
+++ components/Log4NetTester/Program.cs
@@ -0,0 +1,42 @@
+﻿using HPE.HSP.UA3.Core.API.Logger.Loggers;
+using System;
+
+namespace Log4NetTester
+{
+    public class Program
+    {
+        private static CoreLogger logger = new CoreLogger();
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Logging to Windows event log");
+
+                Console.WriteLine("Logging DEBUG");
+                logger.LogDebug("This is a DEBUG test.");
+
+                Console.WriteLine("Logging ERROR");
+                logger.LogError("This is a ERROR test.");
+
+                Console.WriteLine("Logging FATAL");
+                logger.LogFatal("This is a FATAL test.");
+
+                Console.WriteLine("Logging INFO");
+                logger.LogInformational("This is a INFO test.");
+
+                Console.WriteLine("Logging WARN");
+                logger.LogWarning("This is a WARN test.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            finally
+            {
+                Console.Write("Press any key to close...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
