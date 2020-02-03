e6a3d3d8b12d1c23ef060af51401a2c1e17d294e
Raghavan (Ram) Ramanujam <raghavan.ramanujam@gainwelltechnologies.com>
2020-02-03 09:31:57 +0000

    Redis Developer Utility, Solution, PowerShell Scripts and Packages
    Workitems:
    #1256429 Doing:Upgrade the Redis Cache Load tool to support more than one Tenant Details to be uploaded
    #1362949 Code Review for User Story 1256429: Doing:Upgrade the Redis Cache Load tool to support more than one Tenant Details to be uploaded
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1574846

---
+++ components/RedisDeveloperUtility/RedisDeveloperUtility/AzureBlobUploadDownload/BlobUploadNDownload.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDeveloperUtility.AzureBlobUploadDownload
+{
+    class BlobUploadNDownload
+    {
+        public static void BlobManager(string typeOfTenant, string countryCode, string upLoadDownLoad, string env)
+        {
+            if (upLoadDownLoad.ToLower().Equals("download"))
+            {
+                DownloadFile(typeOfTenant, countryCode, env);
+            }
+            else
+            {
+                UploadFile(countryCode, env);
+            }
+        }
+
+        private static void UploadFile(string countryCode, string env)
+        {
+            string storageConnection = string.Empty;
+            string azureDumpFilePath = string.Format(ConfigurationManager.AppSettings["azureDumpFilePath"], env);
+            string azureFolderPath = string.Empty;
+            if (countryCode.ToLower().Equals("us"))
+            {
+                storageConnection = ConfigurationManager.AppSettings["blobStorageUSSASKey"];
+                azureFolderPath = ConfigurationManager.AppSettings["azureUSFolderPath"];
+            }
+            else if (countryCode.ToLower().Equals("in"))
+            {
+                storageConnection = ConfigurationManager.AppSettings["blobStorageINSASKey"];
+                azureFolderPath = ConfigurationManager.AppSettings["azureINFolderPath"];
+            }
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnection);
+
+            string localDumpFilePath = string.Format(ConfigurationManager.AppSettings["localDumpFilePath"], env);
+
+            //create a block blob 
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            //create a container 
+            CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(azureFolderPath);
+            CloudBlockBlob cloudBlockBlob = null;
+            //Deleting the Medicaid Dump Files
+            DeleteFilesFromAzureBlob(cloudBlockBlob, cloudBlobContainer, azureDumpFilePath, "Medicaid");
+
+            Console.WriteLine("Export to Explorer Started :: Medicaid :::" + DateTime.Now);
+            UploadFileToAzureBlob(cloudBlockBlob, cloudBlobContainer, azureDumpFilePath, localDumpFilePath, "Medicaid");
+            Console.WriteLine("Export to Explorer Completed :: Medicaid :::" + DateTime.Now);
+
+            //Deleting the Commercial Dump Files 
+            DeleteFilesFromAzureBlob(cloudBlockBlob, cloudBlobContainer, azureDumpFilePath, "Commercial");
+
+            Console.WriteLine("Export to Explorer Started :: Commercial :::" + DateTime.Now);
+            UploadFileToAzureBlob(cloudBlockBlob, cloudBlobContainer, azureDumpFilePath, localDumpFilePath, "Commercial");
+            Console.WriteLine("Export to Explorer Completed :: Commercial :::" + DateTime.Now);
+        }
+
+        private static void UploadFileToAzureBlob(CloudBlockBlob cloudBlockBlob, CloudBlobContainer cloudBlobContainer, string azureDumpFilePath, string localDumpFilePath, string type)
+        {
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}.rdb", type));
+            cloudBlockBlob.UploadFromFile(localDumpFilePath + string.Format("dump-{0}.rdb", type));
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-5.rdb", type));
+            cloudBlockBlob.UploadFromFile(localDumpFilePath + string.Format("dump-{0}-5.rdb", type));
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-4.rdb", type));
+            cloudBlockBlob.UploadFromFile(localDumpFilePath + string.Format("dump-{0}-4.rdb", type));
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-3.rdb", type));
+            cloudBlockBlob.UploadFromFile(localDumpFilePath + string.Format("dump-{0}-3.rdb", type));
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-2.rdb", type));
+            cloudBlockBlob.UploadFromFile(localDumpFilePath + string.Format("dump-{0}-2.rdb", type));
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-1.rdb", type));
+            cloudBlockBlob.UploadFromFile(localDumpFilePath + string.Format("dump-{0}-1.rdb", type));
+
+        }
+
+        private static void DeleteFilesFromAzureBlob(CloudBlockBlob cloudBlockBlob, CloudBlobContainer cloudBlobContainer, string azureDumpFilePath, string type)
+        {
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-5.rdb", type));
+            if (cloudBlockBlob.Exists())
+            {
+                cloudBlockBlob.Delete();
+            }
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-4.rdb", type));
+            if (cloudBlockBlob.Exists())
+            {
+                cloudBlockBlob.Delete();
+            }
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-3.rdb", type));
+            if (cloudBlockBlob.Exists())
+            {
+                cloudBlockBlob.Delete();
+            }
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-2.rdb", type));
+            if (cloudBlockBlob.Exists())
+            {
+                cloudBlockBlob.Delete();
+            }
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}-1.rdb", type));
+            if (cloudBlockBlob.Exists())
+            {
+                cloudBlockBlob.Delete();
+            }
+            cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(azureDumpFilePath + string.Format("dump-{0}.rdb", type));
+            if (cloudBlockBlob.Exists())
+            {
+                cloudBlockBlob.Delete();
+            }
+        }
+
+        private static void DownloadFile(string typeOfTenant, string countryCode, string env)
+        {
+            string azureDumpFilePath = string.Format(ConfigurationManager.AppSettings["azureDumpFilePath"], env);
+
+            if (!System.IO.Directory.Exists(azureDumpFilePath))
+            {
+                System.IO.Directory.CreateDirectory(azureDumpFilePath);
+            }
+
+            string fileName = string.Empty;
+            string azureFolderPath = string.Empty;
+
+
+            string storageConnection = string.Empty;
+            if (countryCode.ToLower().Equals("us"))
+            {
+                storageConnection = ConfigurationManager.AppSettings["blobStorageUSSASKey"];
+                azureFolderPath = ConfigurationManager.AppSettings["azureUSFolderPath"];
+            }
+            else if (countryCode.ToLower().Equals("in"))
+            {
+                storageConnection = ConfigurationManager.AppSettings["blobStorageINSASKey"];
+                azureFolderPath = ConfigurationManager.AppSettings["azureINFolderPath"];
+            }
+
+            if (typeOfTenant.ToLower().Equals("medicaid"))
+            {
+                fileName = azureDumpFilePath + @"dump-Medicaid.rdb";
+            }
+            else if (typeOfTenant.ToLower().Equals("commercial"))
+            {
+                fileName = azureDumpFilePath + @"dump-Commercial.rdb";
+            }
+            CloudStorageAccount cloudStorageAccount = CloudStorageAccount.Parse(storageConnection);
+
+            //create a block blob 
+            CloudBlobClient cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+
+            try
+            {
+                //create a container 
+                CloudBlobContainer cloudBlobContainer = cloudBlobClient.GetContainerReference(azureFolderPath);
+
+
+                CloudBlockBlob blob = cloudBlobContainer.GetBlockBlobReference(fileName);
+
+                blob.DownloadToFile(fileName, System.IO.FileMode.Create);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("Internal Error Please revisit after some time...");
+                Console.WriteLine("Possible issue: Server might be going through dump refresh");
+                Console.WriteLine(exp.StackTrace);
+            }
+        }
+    }
+}
---
+++ components/RedisDeveloperUtility/RedisDeveloperUtility/UpdateTenantCache/UpdateCache.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDeveloperUtility.UpdateTenantCache
+{
+    class UpdateCache
+    {
+
+        public static void MakeTenantActiveDisableOtherTenants(string tenantGUID)
+        {
+
+            if (!String.IsNullOrEmpty(tenantGUID))
+            {
+                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(ConfigurationManager.AppSettings["redisConnectString"]);
+                IDatabase db = redis.GetDatabase();
+
+                string value = Decompress(db.StringGet(ConfigurationManager.AppSettings["redisTenantKey"]));
+                List<TenantModel> tmp = JsonConvert.DeserializeObject<List<TenantModel>>(value);
+                Guid tenantID = Guid.Parse(tenantGUID);
+                tmp.Where(mod => mod.TenantID != tenantID).ToList().ForEach(x => x.IsActive = false);
+                tmp.Where(mod => mod.TenantID == tenantID).ToList().ForEach(x => x.IsActive = true);
+
+                value = Compress(JsonConvert.SerializeObject(tmp));
+                TimeSpan time = new TimeSpan(720, 60, 60);
+
+                db.StringSet(ConfigurationManager.AppSettings["redisTenantKey"], value, time);
+
+                Environment.Exit(0);
+            }
+            else
+            {
+                Environment.Exit(1);
+            }
+
+        }
+
+
+        public static string Decompress(string compressedText)
+        {
+            byte[] buffer = null;
+            var gzipBuffer = Convert.FromBase64String(compressedText);
+            MemoryStream memoryStream = null;
+
+            try
+            {
+                memoryStream = new MemoryStream();
+                int dataLength = BitConverter.ToInt32(gzipBuffer, 0);
+                memoryStream.Write(gzipBuffer, 4, gzipBuffer.Length - 4);
+                buffer = new byte[dataLength];
+                memoryStream.Position = 0;
+                using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                {
+                    gzipStream.Read(buffer, 0, buffer.Length);
+                }
+
+                return Encoding.UTF8.GetString(buffer);
+            }
+            finally
+            {
+                gzipBuffer = null;
+                buffer = null;
+                if (memoryStream != null)
+                {
+                    memoryStream.Dispose();
+                }
+            }
+        }
+
+        public static string Compress(string text)
+        {
+            byte[] gzipBuffer = null;
+            var buffer = Encoding.UTF8.GetBytes(text);
+            MemoryStream memoryStream = null;
+            try
+            {
+                memoryStream = new MemoryStream();
+                using (var stream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+                {
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+
+                memoryStream.Position = 0;
+                var compressed = new byte[memoryStream.Length];
+                memoryStream.Read(compressed, 0, compressed.Length);
+                gzipBuffer = new byte[compressed.Length + 4];
+                Buffer.BlockCopy(compressed, 0, gzipBuffer, 4, compressed.Length);
+                Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzipBuffer, 0, 4);
+                compressed = null;
+                return Convert.ToBase64String(gzipBuffer);
+            }
+            finally
+            {
+                buffer = null;
+                gzipBuffer = null;
+                if (memoryStream != null)
+                {
+                    memoryStream.Dispose();
+                }
+            }
+        }
+    }
+}
