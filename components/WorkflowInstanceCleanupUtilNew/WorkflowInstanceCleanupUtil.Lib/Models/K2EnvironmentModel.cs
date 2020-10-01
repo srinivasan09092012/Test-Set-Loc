90964e39c163205f5ba9d09acc7966cec333d409
Bibhas Karmakar <bibhas.karmakar@gainwelltechnologies.com>
2020-10-01 12:52:50 +0000

    Missing files added
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1595967

---
+++ components/WorkflowInstanceCleanupUtilNew/WorkflowInstanceCleanupUtil.Lib/Models/K2EnvironmentModel.cs
@@ -0,0 +1,42 @@
+﻿//-----------------------------------------------------------------------------------------
+// This code is the property of DXC Technology, Copyright (c) 2019. All rights reserved.
+//
+// Any unauthorized use in whole or in part without written consent is strictly prohibited.
+// Violators may be punished to the full extent of the law.
+//-----------------------------------------------------------------------------------------
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorkflowInstanceCleanupUtil.Lib.Models
+{
+    public class K2EnvironmentModel
+    {
+        public string Environment { get; set; }
+
+        public string Port { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Server { get; set; }
+
+        public List<K2EnvironmentModel> GetEnvironmentConfiguration()
+        {
+            ICollection<K2EnvironmentModel> environments = new List<K2EnvironmentModel>();
+            var config = WorkflowEnvironmentConfig.GetConfig();
+            foreach (WorkflowEnvironment item in config.Environments)
+            {
+                K2EnvironmentModel workflowEnvironment = new K2EnvironmentModel();
+                workflowEnvironment.Environment = item.Environment;
+                workflowEnvironment.Server = item.Server;
+                workflowEnvironment.UserName = item.Username;
+                workflowEnvironment.Password = item.Password;
+                workflowEnvironment.Port = item.Port;
+                environments.Add(workflowEnvironment);
+            }
+
+            return environments.ToList();
+        }
+    }
+}
---
+++ components/WorkflowInstanceCleanupUtilNew/WorkflowInstanceCleanupUtil.Lib/Workflow/WorkItemCleanUpProcessor.cs
@@ -0,0 +1,77 @@
+﻿//-----------------------------------------------------------------------------------------
+// This code is the property of DXC Technology, Copyright (c) 2019. All rights reserved.
+//
+// Any unauthorized use in whole or in part without written consent is strictly prohibited.
+// Violators may be punished to the full extent of the law.
+//-----------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using WorkflowInstanceCleanupUtil.Lib.Models;
+using K2Mgt = SourceCode.Workflow.Management;
+
+namespace WorkflowInstanceCleanupUtil.Lib.Workflow
+{
+    public class WorkItemCleanUpProcessor : IWorkItemCleanUpProcessor
+    {
+        private K2Mgt.WorkflowManagementServer workflowInstance;
+
+        public WorkItemCleanUpProcessor(string server, uint port, string user, string password, string securityLabel, bool primaryLogin)
+        {
+            try
+            {
+                this.workflowInstance = new K2Mgt.WorkflowManagementServer(server, port, user, password, securityLabel, primaryLogin);
+                this.workflowInstance.Open();
+            }
+            catch (System.Net.Sockets.SocketException exception)
+            {
+                throw new Exception("Error connecting to server - " + exception.Message);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Error occured - " + exception.Message);
+            }
+        }
+
+        public List<ProcessInfo> GetAllProcessInstanceIds(DateTime startDate, DateTime endDate, string processFullName)
+        {
+            K2Mgt.ProcessInstances procinsts = this.workflowInstance.GetProcessInstancesAll(startDate, endDate, processFullName, string.Empty, string.Empty);
+            List<ProcessInfo> list = new List<ProcessInfo>();
+
+            for (int i = 0; i < procinsts.Count; i++)
+            {
+                list.Add(new ProcessInfo(procinsts[i].ID, procinsts[i].Folio));
+            }
+
+            return list;
+        }
+
+        public bool DeleteProcessInstances(int processId, bool deleteLogs = true)
+        {
+            return this.workflowInstance.DeleteProcessInstances(processId, deleteLogs);
+        }
+
+        #region IDisposable
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.workflowInstance != null)
+                {
+                    if (this.workflowInstance.Connection.IsConnected)
+                    {
+                        this.workflowInstance.Connection.Close();
+                    }
+
+                    this.workflowInstance = null;
+                }
+            }
+        }
+        #endregion
+    }
+}
