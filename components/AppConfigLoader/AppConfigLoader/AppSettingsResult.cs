143a0a2ce62d7b33f5b288cb138e61515098bd0a
Sripathi Rao <sripathiramesh.rao@gainwelltechnologies.com>
2020-10-12 05:59:31 +0000

    Added AppSettings Result
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1596811

---
+++ components/AppConfigLoader/AppConfigLoader/AppSettingsResult.cs
@@ -0,0 +1,16 @@
+﻿using FileHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConfigLoader
+{
+    [DelimitedRecord(",")]
+    public class AppSettingsResult : AppSettings
+    {
+        [FieldCaption("Error")]
+        public string Error { get; set; }
+    }
+}
