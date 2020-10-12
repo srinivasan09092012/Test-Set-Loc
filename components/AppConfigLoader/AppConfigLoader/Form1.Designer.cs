1c92c7e2ab9ad0dda9df9e6e3f614ff399b85fd0
Sripathi Rao <sripathiramesh.rao@gainwelltechnologies.com>
2020-10-12 05:57:51 +0000

    AppSetting loader - initial
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1596810

---
+++ components/AppConfigLoader/AppConfigLoader/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace AppConfigLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.LoadAppConfigButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CloudType = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ConnectionString = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTenantId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 171);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Location = new System.Drawing.Point(333, 170);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.BrowseButton.TabIndex = 2;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // LoadAppConfigButton
+            // 
+            this.LoadAppConfigButton.Location = new System.Drawing.Point(323, 214);
+            this.LoadAppConfigButton.Name = "LoadAppConfigButton";
+            this.LoadAppConfigButton.Size = new System.Drawing.Size(95, 23);
+            this.LoadAppConfigButton.TabIndex = 3;
+            this.LoadAppConfigButton.Text = "Load AppConfig";
+            this.LoadAppConfigButton.UseVisualStyleBackColor = true;
+            this.LoadAppConfigButton.Click += new System.EventHandler(this.LoadAppConfigButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(437, 214);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelButton.TabIndex = 4;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "AppConfig Loader ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cloud:";
+            // 
+            // CloudType
+            // 
+            this.CloudType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CloudType.FormattingEnabled = true;
+            this.CloudType.Items.AddRange(new object[] {
+            "Azure",
+            "AWS"});
+            this.CloudType.Location = new System.Drawing.Point(126, 57);
+            this.CloudType.Name = "CloudType";
+            this.CloudType.Size = new System.Drawing.Size(121, 21);
+            this.CloudType.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Connection string: ";
+            // 
+            // ConnectionString
+            // 
+            this.ConnectionString.Location = new System.Drawing.Point(126, 116);
+            this.ConnectionString.Multiline = true;
+            this.ConnectionString.Name = "ConnectionString";
+            this.ConnectionString.Size = new System.Drawing.Size(386, 44);
+            this.ConnectionString.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "TenantId:";
+            // 
+            // txtTenantId
+            // 
+            this.txtTenantId.Location = new System.Drawing.Point(126, 90);
+            this.txtTenantId.Name = "txtTenantId";
+            this.txtTenantId.Size = new System.Drawing.Size(386, 20);
+            this.txtTenantId.TabIndex = 11;
+            this.txtTenantId.Text = "77b50320-5f06-5740-84f4-18d4a8cda51d";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 269);
+            this.Controls.Add(this.txtTenantId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ConnectionString);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CloudType);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.LoadAppConfigButton);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AppConfig Loader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button LoadAppConfigButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CloudType;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ConnectionString;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTenantId;
+    }
+}
+
---
+++ components/AppConfigLoader/AppConfigLoader/Form1.cs
@@ -0,0 +1,197 @@
+﻿using FileHelpers;
+using HPP.Core.API.AppConfig;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AppConfigLoader
+{
+    public partial class Form1 : Form
+    {
+        List<AppSettingsResult> result = new List<AppSettingsResult>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            CloudType.Text = "Azure";
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                Title = "Browse CSV Files",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "csv",
+                Filter = "CSV files (*.csv)|*.csv",
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void LoadAppConfigButton_Click(object sender, EventArgs e)
+        {
+            string label = string.Empty;
+            IAppConfig config = null;
+
+            if (!this.ValidateInput())
+            {
+                return;
+            }
+
+            if (CloudType.Text.Trim() == "Azure")
+            {
+                config = new AzureConfigProvider(ConnectionString.Text.Trim());
+            }
+            else if (CloudType.Text.Trim() == "AWS")
+            {
+                config = new AWSParameterStoreProvider(ConnectionString.Text.Trim());
+            }
+
+            var engine = new DelimitedFileEngine<AppSettings>();
+            engine.Options.IgnoreFirstLines = 1;
+            var appSettings = engine.ReadFileAsList(textBox1.Text);
+            foreach (var item in appSettings)
+            {
+                if (!this.ValidateAppSetting(item))
+                {
+                    continue;
+                }
+
+                label = item.ModuleName + "_" + item.ApplicationName + "_" + txtTenantId.Text.Trim();
+                try
+                {
+                    //config.DeleteAppSetting(item.AppSettingKey, label);
+                    config.AddAppSetting(item.AppSettingKey, item.AppSettingValue, label, true);
+                }
+                catch (Exception ex)
+                {
+                    AppSettingsResult errorItem = new AppSettingsResult()
+                    {
+                        ApplicationName = item.ApplicationName,
+                        ModuleName = item.ModuleName,
+                        AppSettingKey = item.AppSettingKey,
+                        AppSettingValue = item.AppSettingValue,
+                        Error = ex.Message
+                    };
+                    result.Add(errorItem);
+                }
+            }
+
+            if (result.Any())
+            {
+                MessageBox.Show("There are errors while loading appSettings. Please look into 'AppSettingLoad_Errors.csv' file.");
+                string path = Path.GetDirectoryName(textBox1.Text);
+                var errorEngine = new DelimitedFileEngine<AppSettingsResult>();
+                errorEngine.HeaderText = errorEngine.GetFileHeader();
+                errorEngine.WriteFile(path + @"//AppSettingLoad_Errors.csv", result);
+                this.OpenFolder(Path.GetDirectoryName(textBox1.Text));
+            }
+            else
+            {
+                MessageBox.Show("AppConfig loaded successfully!!");
+            }
+        }
+
+        private void OpenFolder(string folderPath)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    Arguments = folderPath,
+                    FileName = "explorer.exe"
+                };
+
+                Process.Start(startInfo);
+            }
+            else
+            {
+                MessageBox.Show(string.Format("{0} Directory does not exist!", folderPath));
+            }
+        }
+
+        private bool ValidateAppSetting(AppSettings item)
+        {
+            string error = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(item.ApplicationName))
+            {
+                error = "ApplicationName is empty; ";
+            }
+
+            if (string.IsNullOrWhiteSpace(item.ModuleName))
+            {
+                error = error + "ModuleName is empty; ";
+            }
+
+            if (string.IsNullOrWhiteSpace(item.AppSettingKey))
+            {
+                error = error + "AppSettingKey is empty;";
+            }
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                AppSettingsResult errorItem = new AppSettingsResult()
+                {
+                    ApplicationName = item.ApplicationName,
+                    ModuleName = item.ModuleName,
+                    AppSettingKey = item.AppSettingKey,
+                    AppSettingValue = item.AppSettingValue,
+                    Error = error
+                };
+                result.Add(errorItem);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidateInput()
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(CloudType.Text.Trim()))
+            {
+                errors.AppendLine("Select Cloud");
+            }
+
+            if (string.IsNullOrWhiteSpace(txtTenantId.Text.Trim()))
+            {
+                errors.AppendLine("Enter valid TenantId");
+            }
+
+            if (string.IsNullOrWhiteSpace(ConnectionString.Text.Trim()))
+            {
+                errors.AppendLine("Enter valid Connection string");
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text.Trim()))
+            {
+                errors.AppendLine("Select appconfig file to load");
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show($"Input validation error: {errors.ToString()}");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
