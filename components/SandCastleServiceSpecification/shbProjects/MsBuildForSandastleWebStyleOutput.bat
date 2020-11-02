d0fa24a42f629ec571436c2ff4156969b9b16bd4
Edmundo Gardea <edmundo.gardea@gainwelltechnologies.com>
2020-11-02 14:30:19 +0000

    Updating msbuild bat file - 29 msbuild instructions
    Workitems:
    #1624552 SandCastle Web Site Support
    #1631718 Updating msbuild bat file - 29 msbuild instructions
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1598820

--- components/SandCastleServiceSpecification/shbProjects/MsBuildForSandastleWebStyleOutput.bat
+++ components/SandCastleServiceSpecification/shbProjects/MsBuildForSandastleWebStyleOutput.bat
@@ -10,7 +10,6 @@ start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\San
 start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\PlanManagement\RelatedInformation\PL_RelatedInformation.shfbproj
 start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\PlanManagement\CodeInformation\PlanM_CodeInformation.shfbproj
 start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderEnrollment\ProviderEnrollment.shfbproj
-start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProgramIntegrity\ProgramIntegrity.shfbproj
 start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderManagement\PManagement.shfbproj
 start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\TaskM\TaskM.shfbproj
 start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\TPLC\TPLCaseTracking.shfbproj
@@ -18,3 +17,16 @@ start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\San
 start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderCredentialing\Audit\ProviderCredentialingAudit.shfbproj
 start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderCredentialing\FileManagement\ProviderCredentialing-FileAdmin.shfbproj
 start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderCredentialing\ValidCredentials\ProviderCredentialing-ValidCredentials.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\Finantial\FM.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\IdentityManagement\IdentityManagement.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderScreening\CLIA\CLIA.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderScreening\Credentials\Credentials.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderScreening\DEA\DEA.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderScreening\LEIES\LEIES.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderScreening\MCSIS\MCSIS.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderScreening\NPPES\NPPES.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderScreening\PECOS\PECOS.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderScreening\SAMCHECK\SAMCHECK.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProviderScreening\SSNDeathMaster\SSNDeathMaster.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProgramIntegrityCT\ProgramIntegrityCT.shfbproj
+start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\ProgramIntegrity\ProgramIntegrity.shfbproj
