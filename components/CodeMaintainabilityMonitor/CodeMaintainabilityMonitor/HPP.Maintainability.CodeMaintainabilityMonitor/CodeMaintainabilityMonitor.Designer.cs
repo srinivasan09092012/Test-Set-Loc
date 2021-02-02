36b0d10b4c8308d2023ff2b91517a1dcaf9ea181
Josue Chavez <josue-omar.chavez@gainwelltechnologies.com>
2021-02-02 18:27:36 +0000

    INX Maintainability automation -  Create Solution
    Workitems:
    #1710178 INX Maintainability automation -  Create Solution
    #1714736 DEV: UX WinForms
    #1720812 Add Maintainability tool to Exclusion on Coverity
    #1720827 INX Maintainability automation -  Create Solution
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1606798

---
+++ components/CodeMaintainabilityMonitor/CodeMaintainabilityMonitor/HPP.Maintainability.CodeMaintainabilityMonitor/CodeMaintainabilityMonitor.Designer.cs
@@ -0,0 +1,53 @@
+﻿//-----------------------------------------------------------------------------------------
+// This code is the property of Gainwell Technologies, Copyright (c) 2021. All rights reserved.
+// Any unauthorized use in whole or in part without written consent is strictly prohibited.
+// Violators may be punished to the full extent of the law.
+//-----------------------------------------------------------------------------------------
+namespace HPP.Maintainability.CodeMaintainabilityMonitor
+{
+    partial class CodeMaintainabilityMonitor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 349);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "CodeMaintainabilityMonitor";
+
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "RadForm1";
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+    }
+}
---
+++ components/CodeMaintainabilityMonitor/CodeMaintainabilityMonitor/HPP.Maintainability.CodeMaintainabilityMonitor/CodeMaintainabilityMonitor.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------------------------
+// This code is the property of Gainwell Technologies, Copyright (c) 2021. All rights reserved.
+// Any unauthorized use in whole or in part without written consent is strictly prohibited.
+// Violators may be punished to the full extent of the law.
+//-----------------------------------------------------------------------------------------
+using System;
+
+namespace HPP.Maintainability.CodeMaintainabilityMonitor
+{
+    public partial class CodeMaintainabilityMonitor : Telerik.WinControls.UI.RadForm
+    {
+        #region Constructors
+        public CodeMaintainabilityMonitor()
+        {
+            this.InitializeComponent();
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+    }
+}
