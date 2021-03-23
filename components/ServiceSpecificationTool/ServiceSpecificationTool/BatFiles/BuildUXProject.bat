a0298de809c764601382e64b9d80daa229b71c8b
v.vempuluri@gainwelltechnologies.com <v.vempuluri@gainwelltechnologies.com>
2021-03-23 10:05:12 +0000

    Remove the extra .xml extention in buildUXproject.bat file.
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1611290

--- components/ServiceSpecificationTool/ServiceSpecificationTool/BatFiles/BuildUXProject.bat
+++ components/ServiceSpecificationTool/ServiceSpecificationTool/BatFiles/BuildUXProject.bat
@@ -20,7 +20,7 @@ For %%A in (%_path%) do (
     set FileName=%%~nA.xml
 ) 
 
-"%MSBUILD%" %ProjectPath% /t:Clean;Rebuild /p:OutDir=%TempDllsOutPutDir% /p:Disable_CopyWebApplication=True,Configuration="Release",DocumentationFile=%FileName%.xml
+"%MSBUILD%" %ProjectPath% /t:Clean;Rebuild /p:OutDir=%TempDllsOutPutDir% /p:Disable_CopyWebApplication=True,Configuration="Release",DocumentationFile=%FileName%
 
 echo "Copying files from Temporary folder to Actual Directory"
 
