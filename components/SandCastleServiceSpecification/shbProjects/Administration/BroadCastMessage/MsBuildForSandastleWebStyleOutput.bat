29a6be3d0ff5d6dc4178710e8a40f297bbfc0b8b
Edmundo Gardea <edmundo.gardea@gainwelltechnologies.com>
2021-08-17 22:59:29 +0000

    shbj proj updates, applicable for all modules
    Workitems:
    #1895976 SandCastle - R22 Publication Enhancement: Create new additional folders (Solution: Current and New folders )to redirect data from one to the other folder to upload to the website
    #1904065 Development
    #1910269 shbj projects - changing target path
    #1904068 deployment and support
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1627204

--- components/SandCastleServiceSpecification/shbProjects/Administration/BroadCastMessage/MsBuildForSandastleWebStyleOutput.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-c:
-cd C:\Program Files (x86)\MSBuild\14.0\Bin
-start MSBuild.exe /p:CleanIntermediates=True /p:Configuration=Release J:\UA3\SandCastleCustomizationTool\ShbDoc\Administration\BroadCastMessage\Administration_BroadCastMessage.shfbproj
