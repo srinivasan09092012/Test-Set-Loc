8557d54b506c8c559ff26c34e521c921fb993739
Saswati Das Datta <saswati.das@gainwelltechnologies.com>
2022-03-08 17:28:07 +0000

    US 2058812 TD: PE Copy Enrollment tool for migration
    Workitems:
    #2056563 PE US1981622: PE Migration Conversion Upgrade - R22 to R23
    #2058812 TD: PE Copy Enrollment tool for migration
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1644880

---
+++ components/PECopyEnrollmentTool/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PECopyEnrollment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+
+            string trackingId = string.Empty;
+            int tenantCode = 0;
+            EnrollmentCopier enrollmentCopier = new EnrollmentCopier();
+
+            while (!endApp)
+            {
+                Console.WriteLine($"Please enter your ATN:");
+                trackingId = Console.ReadLine();
+                trackingId = trackingId.Trim();
+
+                Console.WriteLine($"Please select tenant: Press 1 for Medicaid and Press 2 for Commercial:");
+                tenantCode = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"**********************************************");
+                Console.WriteLine($"PE Application Migration Data Transfer started....");
+                Console.WriteLine("\n");
+
+                try
+                {
+                    enrollmentCopier.Copy(trackingId, tenantCode);
+                    Console.WriteLine($"PE Application Migration Data Transfer completed ||");
+                    Console.WriteLine($"**********************************************");
+
+                    Console.Write("Press 'n' and Enter to close the app, or press Enter to continue: ");
+                    if (Console.ReadLine() == "n") { endApp = true; }
+
+                    Console.WriteLine("\n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+            }
+        }
+    }
+}
---
+++ components/PECopyEnrollmentTool/ProviderEnrollmentDbContext.cs
@@ -0,0 +1,29 @@
+﻿using HP.HSP.UA3.Core.BAS.CQRS.Base;
+using HP.HSP.UA3.Core.BAS.CQRS.Interfaces;
+using Microsoft.Practices.Unity;
+using System.Data.Entity;
+
+namespace PECopyEnrollment
+{
+    public partial class ProviderEnrollmentDbContext : DbContextBase
+    {
+        public ProviderEnrollmentDbContext(IDbSession databaseSession, bool contextOwnsSession = false)
+            : base(databaseSession, contextOwnsSession)
+        {
+            Database.SetInitializer<ProviderEnrollmentDbContext>(new NullDatabaseInitializer<ProviderEnrollmentDbContext>());
+        }
+
+        [InjectionConstructor]
+        public ProviderEnrollmentDbContext(IDbSession databaseSession)
+            : base(databaseSession, false)
+        {
+            Database.SetInitializer<ProviderEnrollmentDbContext>(new NullDatabaseInitializer<ProviderEnrollmentDbContext>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            string schema = this.GetSchemaName("UA3_PROVIDER_ENROLL");
+            modelBuilder.HasDefaultSchema(schema);
+        }
+    }
+}
