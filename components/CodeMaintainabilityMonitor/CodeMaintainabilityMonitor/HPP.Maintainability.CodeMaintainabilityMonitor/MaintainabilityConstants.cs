922a13d433bf3e0b24dca3048405ff2531e1a2e2
Eduardo Corral <eduardo.corral@gainwelltechnologies.com>
2022-03-12 00:24:32 +0000

    Fixing Constants
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1645384

--- components/CodeMaintainabilityMonitor/CodeMaintainabilityMonitor/HPP.Maintainability.CodeMaintainabilityMonitor/MaintainabilityConstants.cs
+++ components/CodeMaintainabilityMonitor/CodeMaintainabilityMonitor/HPP.Maintainability.CodeMaintainabilityMonitor/MaintainabilityConstants.cs
@@ -1,4 +1,9 @@
-﻿namespace HPP.Maintainability.CodeMaintainabilityMonitor
+﻿//-----------------------------------------------------------------------------------------
+// This code is the property of Gainwell Technologies, Copyright (c) 2022. All rights reserved.
+// Any unauthorized use in whole or in part without written consent is strictly prohibited.
+// Violators may be punished to the full extent of the law.
+//-----------------------------------------------------------------------------------------
+namespace HPP.Maintainability.CodeMaintainabilityMonitor
 {
     public static class MaintainabilityConstants
     {
@@ -10,8 +15,16 @@
         public const string MetricsOut = " /out:";
         public const string MetricsExtention = ".xml\"";
         public const string SoluionExention = "*.sln";
-        public const string XmlAttributeValue = "Value";
         public const string XmlSearchPatern = "*.xml";
+
+        public static class DataTableColumnsAndRows
+        {
+            public const string File = "File";
+            public const string Name = "Name";
+            public const string Line = "Line";
+            public const string MaintainabilityIndex = "Maintainability Index";
+        }
+
         public static class Paths
         {
             public const string DoulbeBackslash = "\\";
@@ -23,10 +36,23 @@
             public const string Underscore = "_";
         }
 
+        public static class XmlAttributes
+        {
+            public const string File = "File";
+            public const string Name = "Name";
+            public const string Line = "Line";
+            public const string Value = "Value";
+        }
+
         public static class XmlDescendants
         {
             public const string Method = "Method";
             public const string Metrics = "Metrics";
+        }
+
+        public static class XmlElements
+        {
+            public const string Metric = "Metric";
         }
 
         public static class FiltersForFileFullName
