dafdd6ca9c99769b1e1170f993629e844578f316
Avinash K <avinash.k@gainwelltechnologies.com>
2022-10-04 08:30:53 +0000

    Unity to AutoFac Config conversion
    git-tfs-id: [https://payerportfolio.visualstudio.com/]$/USHC_AMER_US_ADU_HSP_Ua3/Source/Utilities/Dev;C1660743

---
+++ components/UnityToAutoFac/UnityToAutoFac/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace UnityToAutoFac
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(521, 724);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(997, 724);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Validate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1125, 728);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(619, 157);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(663, 561);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 157);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(584, 561);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(892, 724);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Copy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(266, 23);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Module";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(301, 57);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(222, 23);
+            this.comboBox2.TabIndex = 10;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(301, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Type";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(545, 57);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(351, 23);
+            this.comboBox3.TabIndex = 12;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(49, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Unity XML";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(674, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 15);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Autofac Json";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(545, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Application";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1294, 775);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Unity To AutoFac";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Button button3;
+        private ComboBox comboBox1;
+        private Label label2;
+        private ComboBox comboBox2;
+        private Label label3;
+        private ComboBox comboBox3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ components/UnityToAutoFac/UnityToAutoFac/Form1.cs
@@ -0,0 +1,299 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Xml;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace UnityToAutoFac
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            try
+            {
+                xmlDoc.LoadXml(richTextBox2.Text);
+            }
+            catch(Exception ex)
+            {
+                richTextBox1.Text = ex.Message;
+            }
+
+            try
+            {
+                XmlNodeList registers = xmlDoc.GetElementsByTagName("register");
+                string finalText = string.Empty;
+
+                finalText = "{\"components\" : [ ";
+
+                string autoFactext = string.Empty;
+
+                if(registers.Count == 0)
+                {
+                    return;
+                }
+
+                foreach (XmlNode node in registers)
+                {
+                    string type = node.Attributes["type"].Value;
+                    string mapTo = node.Attributes["mapTo"]?.Value;
+
+                    if (string.IsNullOrEmpty(mapTo))
+                    {
+                        mapTo = type;
+                    }
+
+                    XmlAttribute attribute = node.Attributes["name"];
+
+
+                    autoFactext += "{\"type\" : \"" + mapTo + "\",";
+
+                    autoFactext += "\"services\" : [ { \"type\" : \"" + type + "\"";
+
+                    if (attribute != null)
+                    {
+                        autoFactext += ", \"key\" : \"" + attribute.Value + "\"";
+                    }
+                    autoFactext += "}],";
+
+                    XmlNode valueNode = node.FirstChild;
+                    if (valueNode != null)
+                    {
+                        autoFactext += getinstanceScope(valueNode);                     
+                    }
+
+
+                    XmlDocument registerdoc = new XmlDocument();
+                    registerdoc.LoadXml(node.OuterXml);
+
+                    XmlNodeList paramList = registerdoc.GetElementsByTagName("param");
+                    if (paramList?.Count > 0)
+                    {
+                        autoFactext = prepareParams(autoFactext, paramList);
+                    }
+
+                    autoFactext += "},";
+
+                }
+                autoFactext = autoFactext.Remove(autoFactext.Length - 1);
+
+                finalText = finalText + autoFactext + "]}";
+
+                richTextBox1.Text = JsonConvert.DeserializeObject(finalText).ToString();
+            }
+            catch(Exception ex)
+            {
+                richTextBox1.Text = ex.Message;
+            }
+        }
+
+        private string prepareParams(string autoFactext, XmlNodeList paramList)
+        {
+            autoFactext += "\"parameters\" : { ";
+
+            foreach (XmlNode param in paramList)
+            {
+                string name = param.Attributes["name"].Value;
+                string dependencyName = param.Attributes["dependencyName"]?.Value;
+                if(string.IsNullOrEmpty(dependencyName))
+                {
+                    XmlNode valueNode1 = param.FirstChild;
+                    string value1 = valueNode1.Attributes[0].Value;
+                    if (value1.Contains("\\"))
+                    {
+                        value1 = value1.Replace(@"\", @"\\");
+                    }
+
+                    autoFactext += "\"" + name + "\" : ";
+                    autoFactext += "\"" + value1 + "\",";
+                }               
+            }
+            autoFactext = autoFactext.Remove(autoFactext.Length - 1);
+            autoFactext += "}";
+
+            return autoFactext;
+        }
+
+        private string getinstanceScope(XmlNode valueNode)
+        {
+            string value = valueNode.Attributes[0].Value;
+            string retValue = string.Empty;
+           
+            if (value == "transient")
+            {
+                retValue += "\"instanceScope\" : \"per-dependency\",";
+            }
+            if (value == "singleton")
+            {
+                retValue += "\"instanceScope\" : \"single-instance\",";
+            }
+            if (value == "container")
+            {
+                retValue += "\"instanceScope\" : \"per-lifetime-scope\",";
+            }
+            return retValue;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string actualValue = richTextBox1.Text;
+            if (!string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                string value = string.Empty;
+                string result = actualValue;
+                try
+                {
+                    value = JValue.Parse(result).ToString(Formatting.Indented);
+
+                }
+                catch
+                {
+                    value = result;
+                }
+                richTextBox1.Text = value;
+                var test = this.IsValidJson(value);
+                if (test)
+                {
+                    label1.Text = "Valid JSON";
+                    label1.ForeColor = Color.Green;
+                    label1.Visible = true;
+                }
+                else
+                {
+                    label1.Text = "Invalid JSON";
+                    label1.ForeColor = Color.Red;
+                    label1.Visible = true;
+                }
+            }
+        }
+
+        private bool IsValidJson(string strInput)
+        {
+            if (string.IsNullOrWhiteSpace(strInput)) { return false; }
+            strInput = strInput.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
+                (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    var obj = JToken.Parse(strInput);
+                    return true;
+                }
+                catch (JsonReaderException jex)
+                {
+                    return false;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(richTextBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\UA3\PLI Team Implementation\IOC - Containers"); //Assuming Test is your Folder
+
+            FileInfo[] Files = d.GetFiles("*.xml"); //Getting Text files
+            
+            comboBox1.Items.Add("Select");
+            comboBox1.Text = "Select";
+            comboBox2.Items.Add("Select");
+            comboBox2.Text = "Select";
+            HashSet<string> set = new HashSet<string>();
+            foreach (FileInfo file in Files)
+            {
+                set.Add(file.Name.Split('-')[0]);
+            }
+
+            comboBox1.Items.AddRange(set.ToArray());
+
+            comboBox2.Items.Add("UX");
+            comboBox2.Items.Add("BAS");
+            comboBox2.Items.Add("Batch");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadApplications();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadApplications();
+        }
+
+        private void LoadApplications()
+        {
+            string Module = comboBox1.SelectedItem?.ToString();
+            string Type = comboBox2.SelectedItem?.ToString();
+            comboBox3.Items.Clear();
+            comboBox3.Items.Add("Select");
+            comboBox3.Text = "Select";
+
+            if (string.IsNullOrEmpty(Module) || string.IsNullOrEmpty(Type))
+            {
+                return;
+            }
+            if(Type == "UX")
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(@"C:\UA3\PLI Team Implementation\IOC - Containers\" + Module + "-" +Type +".xml" );
+                richTextBox2.Text = xmlDoc.OuterXml;
+                return;
+
+            }
+            DirectoryInfo d = new DirectoryInfo(@"C:\UA3\PLI Team Implementation\IOC - Containers"); 
+
+            FileInfo[] Files = d.GetFiles(Module + "-" +Type + "*.xml"); 
+            
+            HashSet<string> set = new HashSet<string>();
+            foreach (FileInfo file in Files)
+            {
+                
+                if(file.Name.Contains(Module))
+                {
+                    List<string> strings = file.Name.Split('-').ToList();
+                    if(strings.Count > 2)
+                    set.Add(strings[2].Replace(".xml", ""));
+                }
+
+              
+            }
+
+            comboBox3.Items.AddRange(set.ToArray());
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox3.SelectedItem?.ToString() == "Select")
+            {
+                return;
+            }
+            string Module = comboBox1.SelectedItem?.ToString();
+            string Type = comboBox2.SelectedItem?.ToString();
+            if (string.IsNullOrEmpty(Module) || string.IsNullOrEmpty(Type))
+            {
+                return;
+            }
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(@"C:\UA3\PLI Team Implementation\IOC - Containers\" + Module + "-" + Type + "-" + comboBox3.SelectedItem?.ToString() + ".xml");
+            richTextBox2.Text = xmlDoc.OuterXml;
+            return;
+        }
+    }
+}
