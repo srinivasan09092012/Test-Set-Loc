385d9a6700ca73d6c8739aaef6897b6c8126af51
Raghavan Ramanujam (Ram) <97865278+raghavan-ramanujam@users.noreply.github.com>
2023-05-24 14:47:47 +0530

    Pull request for r27 end refactor (#21)
    r27 completed and new branch r27 is created. Main would get converted to
    r28

--- components/ProviderJsonConversion/DataAccess/Properties/AssemblyInfo.cs
+++ components/ProviderJsonConversion/DataAccess/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("23.1.136.0")]
-[assembly: AssemblyFileVersion("23.1.136.0")]
+[assembly: AssemblyVersion("23.2.142.0")]
+[assembly: AssemblyFileVersion("23.2.142.0")]
