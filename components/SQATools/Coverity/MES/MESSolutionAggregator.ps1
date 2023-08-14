7083845e4e179606dfc06d781a8736240fd78752
John Briganti <john.briganti@gainwelltechnologies.com>
2023-08-14 10:24:13 -0600

    2350000 aggregate repo solution lists into one script (#40)
    Co-authored-by: Rafael Martinez <rafael.martinez@gainwelltechnologies.com>

--- components/SQATools/Coverity/MES/MESSolutionAggregator.ps1
+++ components/SQATools/Coverity/MES/MESSolutionAggregator.ps1
@@ -1,5 +1,6 @@
 ﻿# define the output file path
-$outputFile = "C:\UA3\source\mms-cms-util\components\SQATools\Coverity\HSP_UA3_Main(Dev)\Main.CoverityScanList.txt"
+$Branch = $Args
+$outputFile = "C:\UA3\source\mms-cms-util\components\SQATools\Coverity\MES\$Branch\$Branch.CoverityScan.SolutionList.txt"
 
 # make sure the output file is empty before writing to it
 if (Test-Path $outputFile) { Remove-Item $outputFile }
@@ -9,15 +10,15 @@ $normalLines = @()
 $excludedLines = @()
 
 # list of modules
-$modules = "mms-cms-adm", "mms-cms-core", "mms-cms-cfg", "mms-cms-dr", "mms-cms-edi", "mms-cms-fxfer", 
+$modules = "mms-cms-adm", "mms-cms-core", "mms-cms-cfg", "mms-cms-dr", "mms-cms-edi", "mms-cms-fxfer",
             "mms-cms-im", "mms-cms-inx", "mms-cms-mc", "mms-hp-mco", "mms-cms-note", "mms-cms-pl",
             "mms-cms-pict", "mms-cms-pc", "mms-cms-pe", "mms-cms-pm", "mms-hp-prov", "mms-cms-ss",
             "mms-cms-tm", "mms-cms-tplct", "mms-cms-tplp", "mms-hp-mbr", "mms-hp-dr", "mms-cms-pi",
-            "mms-cms-fm", "mms-cms-authdeter", "mms-cms-cef", "mms-cms-fm", "mms-cms-meddent", 
-            "mms-cms-paae", "mms-cms"
+            "mms-cms-fm", "mms-cms-authdeter"
 
 # iterate over each module
 foreach ($module in $modules) {
+
     # get all SolutionList.txt files in the current module directory
     $files = Get-ChildItem "C:\UA3\source\$module\architecture\component-lists" -Filter "*.SolutionList.txt"
 
@@ -26,12 +27,15 @@ foreach ($module in $modules) {
         $content = Get-Content $file.FullName
 
         # split lines into normal and excluded ones
-        foreach ($line in $content) {
+        foreach ($line in $content) 
+        {
             if ($line -ne "#Exclude from scanning") {
+
                 if ($line.StartsWith("#")) {
                     $excludedLines += $line
                 } else {
-                    $normalLines += $line
+                    # add "/src/main-" before the line content
+                    $normalLines += "/src/$Branch-" + $line
                 }
             }
         }
